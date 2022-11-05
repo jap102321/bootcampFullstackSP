ccf61d608987b3a0e7432bb9adc6c1097930b8ed
Juan Pablo <jupago14@gmail.com>
2022-11-04 19:27:40 -0500

    2.a ended

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -1,33 +1,54 @@
+import java.util.Scanner;
+
 public class Challenge1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int i=9;
-
         AleatoryString ale = new AleatoryString();
 
-        System.out.println(ale.generateNumber(i));
+        System.out.println("Please type in the type of data that you want: A & B");
+        String type = sc.next();
+        System.out.println(ale.generateNumber(type));
     }
 }
 
-class AleatoryString{
+class AleatoryString {
     String aleatory;
 
 
-    public AleatoryString(){
+    public AleatoryString() {
 
     }
 
 
+    public String generateNumber(String typeOf) {
 
-    public String generateNumber(int i){
-        String bank= "0123456789";
+        String bank = "0123456789";
+        int longitud = 8;
         StringBuilder builder;
-        builder = new StringBuilder(i);
-        for(int x = 0; x < i;x++){
-            int index = (int) (bank.length()*Math.random());
-            builder.append(bank.charAt(index));
-        }
+        builder = new StringBuilder(longitud);
 
+        if (typeOf.equalsIgnoreCase("A")) {
+            aleatory = "54";
+            builder = builder.append(aleatory);
+            for (int x = 0; x < longitud; x++) {
+                double Random = Math.random() * bank.length();
+                int pos = (int) Random;
+                char letter = bank.charAt(pos);
+                builder = builder.append(letter);
+            }
+        } else if (typeOf.equalsIgnoreCase("B")) {
+            aleatory = "08";
+            builder = builder.append(aleatory);
+            for (int x = 0; x < longitud; x++) {
+                double Random = Math.random() * bank.length();
+                int pos = (int) Random;
+                char letter = bank.charAt(pos);
+                builder = builder.append(letter);
+            }
+        }else{
+            return "You didn't indicate the right type of n°";
+        }
         return builder.toString();
     }
-
 }
