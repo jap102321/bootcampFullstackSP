32e9a3c32e397e00d44882d5900dc517c270d84f
Juan Pablo <jupago14@gmail.com>
2022-11-09 19:19:47 -0500

    corrections made

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -2,33 +2,36 @@ import java.util.*;
 
 public class Challenge1 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         AleatoryString ale = new AleatoryString();
-
-        System.out.print("Please type in the type of data that you want (A & B): ");
-        String type = sc.next();
-        System.out.println(ale.generateNumber(type));
+        LineDatabase ld = new LineDatabase();
 
 
 
-        //
-        List<String> data;
-        data = new ArrayList<>();
-        data.add("PERÚ");
-        data.add("PERU");
-        data.add("BOLIVIA");
-        data.add("CHILE");
-        data.add("ARGENTINA");
-        data.add("PARAGUAY");
-        data.add("URUGUAY");
-        data.add("BRASIL");
-        data.add("MÉXICO");
-        data.add("MEXICO");
+        System.out.print("How many new lines do you want to generate and add to " +
+                "the database: ");
+        int newLines = sc.nextInt();
+
+        String [] linesData = new String[newLines];
+
+        for(int i = 0 ; i < newLines; i++){
+            System.out.print("Please type in the type of data that you want (A & B): ");
+            String type = sc.next();
+            linesData[i]=ale.generateNumber(type);
+            ld.addLineToDatabase(linesData[i]);
+            System.out.println(linesData[i]);
+        }
 
 
-        System.out.print("Give me the name of a wine producer country from latinamerica: ");
-        String dataPos = sc.next();
-        System.out.println(data.contains(dataPos.toUpperCase()));
+        System.out.println("The numbers in the DB are " + Arrays.toString(linesData));
+        System.out.print("Check is a number is available: ");
+        String numberInDB= sc.next();
+        System.out.println(ld.getInfoDB(numberInDB));
+
+
+
+
 
     }
 }
@@ -41,9 +44,7 @@ class AleatoryString {
 
     }
 
-
     public String generateNumber(String typeOf) {
-
         String bank = "0123456789";
         int longitud = 8;
         StringBuilder builder;
@@ -72,5 +73,25 @@ class AleatoryString {
         }
         return builder.toString();
     }
+
+}
+
+class LineDatabase{
+    private String aleatory,numberInDB;
+    private ArrayList<String> line = new ArrayList<>();
+
+    public boolean getInfoDB(String numberInDB){
+        if(line.contains(numberInDB)){
+            return false;
+        }
+
+        return true;
+    }
+    public void addLineToDatabase(String aleatory){
+        this.numberInDB=aleatory;
+        line.add(aleatory);
+
+    }
+
 }
 
